4ab692d41953c673d0ef2e635ff821e1ac5c97e7
papafreshceo <papafresh.ceo@gmail.com>
2025-10-03 10:13:14 +0900

    1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,15 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "no-unused-vars": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
