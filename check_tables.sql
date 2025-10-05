d37cf6956405c11489d340ac71e5ebfc97020170
papafreshceo <papafresh.ceo@gmail.com>
2025-10-06 08:37:58 +0900

    1

--- check_tables.sql
+++
@@ -1,6 +0,0 @@
--- 현재 테이블 확인
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = 'public' 
-AND table_name LIKE '%partner%' OR table_name LIKE '%supplier%'
-ORDER BY table_name;
